04453ab07f7cfb737b7e491c0c7bb89951bb6f06
Dany <danialikhani@gmail.com>
2024-03-13 15:25:08 +0100

    add API_weather

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 import express from "express";
 
-const HOST = process.env.HOST
-const PORT = process.env.PORT
+const API_KEY =process.env.API_KEY
+const HOST = process.env.HOST;
+const PORT = process.env.PORT;
 const app = express();
-const enviornment = process.env.NODE_ENV
+const enviornment = process.env.NODE_ENV;
 
 app.get("/", (req, res) => {
   res.send("welcome");
@@ -11,19 +12,26 @@ app.get("/", (req, res) => {
 
 app.get("/random-number", (req, res) => {
   const number = Math.floor(Math.random() * 100);
-  res.json({value: number});
+  res.json({ value: number });
 });
 
 app.get("/greeting", (req, res) => {
-	const greetings = ["hey", "hi", "hola", "ciao"];
-	const greetingIndex = Math.floor(Math.random() * greetings.length);
-	res.json({ value: greetings[greetingIndex] })
+  const greetings = ["hey", "hi", "hola", "ciao"];
+  const greetingIndex = Math.floor(Math.random() * greetings.length);
+  res.json({ value: greetings[greetingIndex] });
+});
+
+app.get("/weather/:location", async (req, res) => {
+	const response = await fetch(`https://api.tomorrow.io/v4/weather/forecast?location=${req.params.location}&apikey=${API_KEY}`);
+	const data = await response.json();
+	res.send("Current temperature: " + data.timelines.minutely[0].values.temperature);
+	// res.json(data);
 })
 
 app.listen(PORT, () => {
-    if (enviornment === "development") {
-        console.log(`server running om ${HOST}:${PORT}`)
-    } else {
-  console.log(`server running o production`);
-    }
+  if (enviornment === "development") {
+    console.log(`server running om ${HOST}:${PORT}`);
+  } else {
+    console.log(`server running o production`);
+  }
 });
